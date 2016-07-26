88775132fb1e0c94affab0080c8de85076d6828f
Ba Manzi <bamanzi@gmail.com>
2016-07-26 20:55:12 +0800

    update make_mxt3.sh to build emacs-24.3 plugin

--- make_mxt3.sh
+++ make_mxt3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ZIP=`which zip || echo "../zip"`
-for d in emacs-24.5 emacs-ext-24.5; do
-  (cd $d && $ZIP -r -1 ../$d.mxt3 *)
+ZIP=`which zip || echo "./busybox zip"`
+for d in emacs-*; do
+  ( [ -d $d ] && cd $d && $ZIP -r -1 ../$d.mxt3 *)
 done 
